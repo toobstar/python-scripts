2873696785f15a82f63ed033d3b06e2ca962724a
Toby Vidler <toby.vidler@prospection.com>
2025-04-30 16:47:52 +1000

    fix constants, and simplify pass by reference with dict

--- log_parser.py
+++ log_parser.py
@@ -1,11 +1,15 @@
 import requests
 import re
 
+# Fancy tables in logs: https://rich.readthedocs.io/en/latest/tables.html
 from rich.console import Console
 from rich.table import Table
 
 # sample OpenSSH log 
-url = 'https://raw.githubusercontent.com/logpai/loghub/refs/heads/master/OpenSSH/OpenSSH_2k.log'
+URL = 'https://raw.githubusercontent.com/logpai/loghub/refs/heads/master/OpenSSH/OpenSSH_2k.log'
+
+MISSING = '<missing>'
+FAILED_PASSWORD_PHRASE = 'Failed password'
 
 # use a streaming request API to better support larger files
 def get_stream(url, log_output_processor, state_register, callback):
@@ -29,7 +33,7 @@ def find_ip_address(line):
     match = re.search(pattern, line)
     if match:
         return match.group(0)
-    return '<missing>'
+    return MISSING
 
 def find_username(line):
     if 'user' in line:
@@ -37,20 +41,19 @@ def find_username(line):
         match = re.search(pattern, line)
         if match:
             return match.group(0)    
-    return '<missing>'
+    return MISSING
 
-def log_line_processor(line, state_register):
-    # Example:
-    #   Dec 10 07:07:45 LabSZ sshd[24206]: Failed password for invalid user test9 from
-    
-    if 'Failed password' in line:
+def log_line_processor(line, state_register):    
+    if FAILED_PASSWORD_PHRASE in line:
       ip_address = find_ip_address(line)
       username = find_username(line)
 
-      failed_ip_addresses_for_user = state_register.get(username, {})
+      if username not in state_register:
+          state_register[username] = {}
+
+      failed_ip_addresses_for_user = state_register[username]
       ip_count = failed_ip_addresses_for_user.get(ip_address, 0)
       failed_ip_addresses_for_user[ip_address] = ip_count + 1
-      state_register[username] = failed_ip_addresses_for_user
     
 def report_analysis(register_failed_logins):
     table = Table(title="Log Report")
@@ -61,7 +64,7 @@ def report_analysis(register_failed_logins):
     for username, ip_addresses in register_failed_logins.items():
         print(f"User: {username}")
         for ip_address, count in ip_addresses.items():
-            print(f"  IP Address: {ip_address}, Count: {count}")        
+            print(f"  IP Address: {ip_address}, Count: {count}")       
             table.add_row(username, ip_address, str(count))
     
     print("Total failed logins:", sum([sum(ip_addresses.values()) for ip_addresses in register_failed_logins.values()]))
@@ -73,6 +76,5 @@ def report_analysis(register_failed_logins):
     
     
 if __name__ == "__main__":
-  register_failed_logins = {}
-  get_stream(url, log_line_processor, register_failed_logins, report_analysis)
+  get_stream(URL, log_line_processor, {}, report_analysis)
   
