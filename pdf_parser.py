5c374b678e02df553678c03e7343a5a47d2db49c
Toby Vidler <toby.vidler@prospection.com>
2025-05-06 12:36:51 +1000

    invoice parser implementations

--- pdf_parser.py
+++ pdf_parser.py
@@ -1,12 +1,17 @@
+import os
 import sys
 import pymupdf4llm
 import pathlib
+from openai import OpenAI
 
+
+PROMPT = "If the following text is an invoice can you return a JSON modelled object containing all the elements you can identify?  \n\n"
 
 def is_invoice(text: str) -> bool:
     invoice_indicators = [
-        'invoice', 'bill', 'payment', 'due date', 'total amount',
-        'invoice number', 'invoice date', 'billed to', 'payment terms'
+        'bill', 'billed to', 'payment terms', 'tax', 'GST', 'VAT'
+        'invoice', 'invoice date', 'payment', 'due date', 'total amount',
+        
     ]
     text_lower = text.lower()
 
@@ -26,7 +31,59 @@ def parse_file(file_path: str, write_markdown: bool) -> str:
     return md_text
 
 
+def query_openai_text(text: str) -> str:
+    # client = OpenAI(api_key = os.environ.get("OPENAI_API_KEY"))
+    client = OpenAI()
+    # response = client.responses.create(
+    #     model="gpt-4.1",
+    #     input="Write a one-sentence bedtime story about a unicorn."
+    # )
+    # print(text, response.output_text)
+
+    response = client.responses.create(
+        model="o4-mini",
+        reasoning={"effort": "medium"},
+        input=[
+            {
+                "role": "user", 
+                "content": PROMPT + text
+            }
+        ]
+    )
+
+    return response.output_text
+
+
+def query_openai_pdf(file_path: str):
+    client = OpenAI()
+
+    file = client.files.create(
+        file=open(file_path, "rb"),
+        purpose="user_data"
+    )
+
+    response = client.responses.create(
+        model="gpt-4.1",
+        input=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "input_file",
+                        "file_id": file.id,
+                    },
+                    {
+                        "type": "input_text",
+                        "text": PROMPT,
+                    },
+                ]
+            }
+        ]
+    )
+    return response.output_text
+
 def main():
+        
     if len(sys.argv) > 1:
         file_path = sys.argv[1]
     else:
@@ -37,7 +94,10 @@ def main():
 
     try: 
         markdown = parse_file(file_path, False)
-        print("is invoice? ", is_invoice(markdown))
+
+        print("Method 1: basic pattern matching ", is_invoice(markdown))
+        print("Method 2: openai text parsing     ", query_openai_text(markdown))
+        print("Method 3: openai PDF parsing", query_openai_pdf('static/invoice.pdf'))
 
     except (FileNotFoundError, IOError) as e:
         print(e)
